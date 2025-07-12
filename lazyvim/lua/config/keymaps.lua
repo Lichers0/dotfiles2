b382f322dfe7526b8b3a5a7d7c0063f07a3a9963
Denis Romodonov <romodanov.denis@gmail.com>
2025-07-12 17:56:03 +0300

    [keys] Path to clipboard

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>bn", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+  print("Copied: " .. vim.fn.expand("%:p"))
+end, { desc = "Copy file path" })
+
+vim.keymap.set("n", "<leader>bm", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+  print("Copied: " .. vim.fn.expand("%"))
+end, { desc = "Copy relative path" })
