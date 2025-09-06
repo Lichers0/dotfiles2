ea443b3412ab1260ca29bccac5a2629693fea2e8
romodanov.d <romodanov.d@MacBook-Air-romodanovd.local>
2025-09-06 17:29:37 +0300

    Add bufferline plugin

---
+++ lazyvim/lua/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  keys = {
+    { "<leader>bj", "<cmd>BufferLinePick<cr>", desc = "Jump to buffer" },
+  },
+}
